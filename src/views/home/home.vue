ac3a55780b224e3e4e1cbcf0c2c72889cdfd4193
DESKTOP-KRK8038\Chen <18382310570@163.com>
2020-03-06 19:34:46 +0800

    '添加轮播图'

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,37 +1,46 @@
 <template>
-    <div id="home-nav">
-        <nav-bar class="home-nav">
-            <div slot="center">购物街</div>
-        </nav-bar>
-    </div>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
+
+    <home-swiper :banners="banners"/>
+
+
+  </div>
 </template>
 
 <script>
-    import NavBar from "components/common/navbar/NavBar";
-    import {getHomeMultiData} from "network/home"
-    export default {
-        name: "home",
-        components: {
-            NavBar
-        },
-        data(){
-            return {
-                banners:[],
-                recommends:[]
-            }
-        },
-        created() {
-            getHomeMultiData().then(res=>{
-                this.banners = res.data.banner.list;
-                this.recommends = res.data.recommend.list;
-            })
-        }
+  import NavBar from "components/common/navbar/NavBar";
+  import HomeSwiper from "views/home/childComps/HomeSwiper";
+
+
+  import {getHomeMultiData} from "network/home"
+
+
+  export default {
+    name: "home",
+    components: {
+      NavBar, HomeSwiper
+    },
+    data() {
+      return {
+        banners: [],
+        recommends: []
+      }
+    },
+    created() {
+      getHomeMultiData().then(res => {
+        this.banners = res.data.banner.list;
+        this.recommends = res.data.recommend.list;
+      })
     }
+  }
 </script>
 
 <style scoped>
-    .home-nav {
-        background-color: var(--color-tint);
-        color: #fff;
-    }
+  .home-nav {
+    background-color: var(--color-tint);
+    color: #fff;
+  }
 </style>
